b2729baf9a77d9ad84901bee6b0f3b0a5c6e6317
goodven152 <goodven152@gmail.com>
2025-07-10 15:39:39 +0400

    Рефакторинг кода: вынесение логики открытия кейса в отдельную функцию и улучшение структуры основного цикла

--- main.py
+++ main.py
@@ -3,53 +3,67 @@ import time
 
 items = ["knife", "ak-47", "gloves"]
 inventory = []
+case_balance = 1
 
-while True:
-    print("==== Добро пожаловать в симулятор кейсов ====")
-    print("\n\n\n\n")
-    print("Выберите, что вы хотите сделать:")
-    print("1. Открыть кейс\t2.Посмотреть инвентарь\t3.Выйти\n")
-    number = input("Введите цифру:")
-    if number == "1":
-        print("Открываем кейс...")
-        time.sleep(5)
-        drop = random.choice(items)
-        print("\n")
-        print("\n")
-        print("\n")
-        print(f"Выпал предмет {drop}\n")
-        print("1.Продать\t2.Оставить\n")
-        number = input("Введите число:")
+def open_case():
+    global case_balance
+    print("Открываем кейс...")
+    case_balance -= 1
+    time.sleep(5)
+    drop = random.choice(items)
+    print("\n")
+    print("\n")
+    print("\n")
+    print(f"Выпал предмет {drop}\n")
+    print(f"У вас осталось {case_balance} кейса(ов)")
+    return drop
+
+
+def main():
+    while True:
+        print("==== Добро пожаловать в симулятор кейсов ====")
+        print("\n\n\n\n")
+        print(f"У вас имеется {case_balance}, кейс(а)\n")
+        print("Выберите, что вы хотите сделать:\n")
+        print("1. Открыть кейс\t2.Посмотреть инвентарь\t3.Выйти\n")
+        number = input("Введите цифру:")
         if number == "1":
-            drop = 0
-            print("Вы продали предмет! Выберите следующее действие\n")
-            print("1. Вернуться на главную.\t2. Выйти")
-            number = input("Введите число: ")
-            if number == "1":
-                continue
+            if case_balance != 0:
+                drop = open_case()
+                print("1.Продать\t2.Оставить\n")
+                number = input("Введите число:")
+                if number == "1":
+                    print("Вы продали предмет! Выберите следующее действие\n")
+                    print("1. Вернуться на главную.\t2. Выйти")
+                    number = input("Введите число: ")
+                    if number == "1":
+                        continue
+                    else:
+                        exit()
+                elif number == "2":
+                    inventory.append(drop)
+                    print("Предмет в инвентаре! Выберите действие\n")
+                    print("1. Вернуться на главную\t2. Выйти")
+                    number = input("Введите число: ")
+                    if number == "1":
+                        continue
+                    elif number == "2":
+                        break
+                    else:
+                        print("Введите число!")
+                else:
+                    print("Введите число!")
             else:
-                exit()
+                print("У вас нет кейсов!")
+                continue
         elif number == "2":
-            inventory.append(drop)
-            print("Предмет в инвентаре! Выберите действие\n")
-            print("1. Вернуться на главную\t2. Выйти")
+            print("Ваш инвентарь:\n")
+            print(inventory)
+            print("1.Вернуться назад.\t2.Выйти\n")
             number = input("Введите число: ")
-            if number == "1":
-                continue
-            elif number == "2":
-                exit()
-            else:
-                print("Введите число!")
+            continue
+        elif number == "3":
+            exit()
         else:
-            print("Введите число!")
-    elif number == "2":
-        print("Ваш инвентарь:\n")
-        print(inventory)
-        print("1.Вернуться назад.\t2.Выйти\n")
-        number = input("Введите число: ")
-        continue
-    elif number == "3":
-        exit()
-    else:
-        print("Неверное число")
-        exit()
+            print("Неверное число")
+            break
